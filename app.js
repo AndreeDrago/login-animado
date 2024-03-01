d5e7466f3d4e9004fdfcb0f67894cb447e9e95da
Andree Drago <andreefx@hotmail.com>
2024-02-29 18:47:57 -0600

    completo

---
+++ app.js
@@ -0,0 +1,8 @@
+let labels = document.querySelectorAll('.formularioSeccion label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+})
